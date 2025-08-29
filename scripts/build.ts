4c2303cb86b8d0388f1698cc902b270c37d9aa50
Đăng Minh <dangminh2208@gmail.com>
2025-08-29 20:18:08 +0200

    deploy

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,7 +4,7 @@ import { join, basename } from "path";
 // ----------------------------
 // Configuration
 // ----------------------------
-const OUT_DIR = ".out";
+const OUT_DIR = "out";
 mkdirSync(OUT_DIR, { recursive: true });
 
 // ----------------------------
